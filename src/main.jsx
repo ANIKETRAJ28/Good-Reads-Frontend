c0f64de87061cb5ab39a1bd1ddb1811930279226
Aniket <aniketraj28042003@gmail.com>
2024-07-17 13:16:08 +0530

    basic redux setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import './index.css';
 
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from 'Redux/store';
 
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
