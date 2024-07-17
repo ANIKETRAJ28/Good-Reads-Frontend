dd123a8760521570d215137e277af2883747d43e
Aniket <aniketraj28042003@gmail.com>
2024-07-17 14:47:05 +0530

    added home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,21 @@
+import Logo from "Assets/BookShelf.png";
+
 export default function Home() {
     return (
-        <>Home Component</>
+        <div className="flex flex-col items-center justify-evenly h-[80vh]">
+            <div className="h-56 w-52">
+                <img className="w-full h-full" src={Logo} alt="logo" />
+            </div>
+            <div className="text-3xl flex items-center justify-around">
+                <div className="flex flex-col items-center gap-6 basis-2/5 font-semibold leading-[3rem] tracking-[0.4rem]">
+                    <h1 className="uppercase text-white">book shelf</h1>
+                    <div className="capitalize text-center text-warning">your personal library and social network for bookworms</div>
+                </div>
+                <div className="text-lg text-black flex gap-4">
+                    <button className="capitalize btn btn-primary px-4 py-2 rounded-md font-semibold">register</button>
+                    <button className="capitalize btn btn-warning px-4 py-2 rounded-md font-semibold">login</button>
+                </div>
+            </div>
+        </div>
     );
 }
