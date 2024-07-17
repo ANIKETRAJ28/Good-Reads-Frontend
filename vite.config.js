ec66858f0999e11d51e38a8a3627a8c53392b9fc
Aniket <aniketraj28042003@gmail.com>
2024-07-17 13:01:41 +0530

    added absolute import configurations using vite.config and js.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      Components: "/src/Compoennts",
+      Assets: "/src/Assets",
+      Pages: "/src/Pages",
+      Helpers: "/src/Helpers",
+      Config: "/src/Config",
+      Hooks: "/src/Hooks",
+      Redux: "/src/Redux",
+      Routes: "/src/Routes",
+      Layout: "/src/Layout"
+    }
+  }
+});
